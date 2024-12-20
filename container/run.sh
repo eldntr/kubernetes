7f74d9c981d992e602b5f057e7138dc290c91750
Eldintaro Farrandi <eldintarofarrandi@gmail.com>
2024-12-20 13:30:05 +0700

    deleted:    container/run.sh 	deleted:    container/setup.sh 	deleted:    cronjob.yaml 	deleted:    rollout-restart-role.yaml

--- container/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export KUBECONFIG=$HOME/.kube/config
-kubectl apply -f ../k8s/deployment.yaml
-kubectl apply -f ../k8s/service.yaml
-# Restart deployment to use the latest image
-kubectl rollout restart deployment/nginx-rtmp-deployment
